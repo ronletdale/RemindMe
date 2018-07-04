fdf82965ed5528b333f2a2e1c1e1a56caacb4bf2
nrepnin <nrepnin@gmail.com>
2018-07-04 19:33:45 +0300

    Changed layout. Added Navigation View.

--- app/src/main/java/com/example/nrepnin/remindme/MainActivity.java
+++ app/src/main/java/com/example/nrepnin/remindme/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.nrepnin.remindme;
 
 import android.app.Activity;
+
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 
 }
